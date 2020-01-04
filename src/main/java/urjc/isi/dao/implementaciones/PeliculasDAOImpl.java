3ff8290577b2a298c14c6318325cdbfd4665e667
oscarescuderoarnanz <escuderoarnanzoscar@gmail.com>
2020-01-04 16:33:42 +0100

    Prueba dada duracion 139.0

--- src/main/java/urjc/isi/dao/implementaciones/PeliculasDAOImpl.java
+++ src/main/java/urjc/isi/dao/implementaciones/PeliculasDAOImpl.java
@@ -134,7 +134,7 @@ public class PeliculasDAOImpl extends GenericDAOImpl<Peliculas> implements Pelic
 	@Override
 	public List<Peliculas> selectAllDuration(double value) {
 	  List<Peliculas> filmList = new ArrayList<>();
-	  String sql = "SELECT * from peliculas " + "where duracion=" + 139.0;
+	  String sql = "SELECT * from peliculas " + "where duracion=" + value;
 	  
 	  try (PreparedStatement pstmt = c.prepareStatement(sql)) {
 		  ResultSet rs = pstmt.executeQuery();
