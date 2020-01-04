237cec84d44ea5810508dad5220a76df2c756f6b
oscarescuderoarnanz <escuderoarnanzoscar@gmail.com>
2020-01-04 15:31:26 +0100

    Nueva prueba

--- src/main/java/urjc/isi/dao/implementaciones/PeliculasDAOImpl.java
+++ src/main/java/urjc/isi/dao/implementaciones/PeliculasDAOImpl.java
@@ -134,15 +134,15 @@ public class PeliculasDAOImpl extends GenericDAOImpl<Peliculas> implements Pelic
 	@Override
 	public List<Peliculas> selectAllDuration(double value) {
 	  List<Peliculas> filmList = new ArrayList<>();
-	  String sql = "SELECT duracion from peliculas " +
+	  String sql = "SELECT * from peliculas" +
 			  	"where duracion=" + value;
 	  
 	  try (PreparedStatement pstmt = c.prepareStatement(sql)) {
 		  ResultSet rs = pstmt.executeQuery();
 		  c.commit();
-		  //while(rs.next()){
-		  filmList.add(fromResultSet(rs));
-		  //}
+			  while(rs.next()){
+			  filmList.add(fromResultSet(rs));
+		  }
 	  } catch (SQLException e) {
 		  System.out.println(e.getMessage());
 	  }
