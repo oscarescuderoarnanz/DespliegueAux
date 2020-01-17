47b9b3583acb55288f76f6ba25d2f488a5ee87d3
oscarescuderoarnanz <escuderoarnanzoscar@gmail.com>
2020-01-17 14:28:01 +0100

    Prueba duracion ordenada

--- src/main/java/urjc/isi/dao/implementaciones/PeliculasDAOImpl.java
+++ src/main/java/urjc/isi/dao/implementaciones/PeliculasDAOImpl.java
@@ -142,7 +142,9 @@ public class PeliculasDAOImpl extends GenericDAOImpl<Peliculas> implements Pelic
 						 "Inner join guionistas as g on pg.idpersona=g.idpersona ";
 					cond+= "g.fullnombre LIKE "+"'"+conditions.get("guionista")+"'";
 					break;
-				case "duracion":					
+				case "duracion":	
+					add_order = true;
+					order += "p.duracion DESC";
 					if(conditions.get("duracion").indexOf("<") == 0) {
 						cond+= "p.duracion <= "+"'"+conditions.get("duracion").split("<")[1]+"'";
 						break;
